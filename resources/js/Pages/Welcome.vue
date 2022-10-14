c97bccc40bcae0fbc720c2e4fdf744a4f8924447
MwauraKimani <kimmwaus@gmail.com>
2022-10-14 22:09:38 +0300

    Initial commit

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,34 @@
+<template>
+    <nav>
+        <div class="desktop-nav">
+            <div class="log-section"></div>
+            <div class="button-section"></div>
+            <div class="contact-button"></div>
+        </div>
+        <div class="mobile-nav">
+
+        </div>
+    </nav>
+</template>
+
+<script>
+
+export default {
+    name:"welcome"
+}
+
+</script >
+
+<style lang="scss" >
+
+.desktop-nav{
+    width: 100%;
+    height: 60px;
+    background-color: red;
+    /*.button-section{*/
+
+    /*}*/
+}
+
+
+</style>
