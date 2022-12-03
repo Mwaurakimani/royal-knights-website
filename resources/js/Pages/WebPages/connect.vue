6f85fe754a7898e51c2921da8f0dad9fc48318af
MwauraKimani <kimmwaus@gmail.com>
2022-12-03 12:46:25 +0300

    update to contact us

--- resources/js/Pages/WebPages/connect.vue
+++ resources/js/Pages/WebPages/connect.vue
@@ -1,6 +1,43 @@
 <template>
     <website-layout :activeLink="5">
         <top-banner :pageName="pageName"></top-banner>
+        <div class="banner-background">
+            <h1>GET IN TOUCH</h1>
+            <div class="contact-det">
+                <section>
+                    <div class="icon-holder">
+                    </div>
+                    <label for=""> Phone :</label>
+                    <p>+245 700 000 000</p>
+                </section>
+                <section>
+                    <div class="icon-holder">
+                    </div>
+                    <label for="">Email :</label>
+                    <p>email@email.com</p>
+                </section>
+                <section>
+                    <div class="icon-holder">
+                    </div>
+                    <label for="">Address :</label>
+                    <p>Nairobi CBD Times tower</p>
+                </section>
+
+            </div>
+            <div class="contact-form">
+                <div class="user-details">
+                    <input type="text" placeholder="Name">
+                    <input type="text" placeholder="Email">
+                    <input type="text" placeholder="Subject">
+                </div>
+                <div class="text-area">
+                    <textarea placeholder="Enter Message..."></textarea>
+                </div>
+                <div class="button-section">
+                    <button class="button-link" >SEND MESSAGE</button>
+                </div>
+            </div>
+        </div>
     </website-layout>
 </template>
 
@@ -23,7 +60,4 @@ export default {
 }
 </script>
 
-<style lang="scss" >
-@use "../resources/scss/index";
-
-</style>
+<style lang="scss" scoped src="./connect.scss" ></style>
