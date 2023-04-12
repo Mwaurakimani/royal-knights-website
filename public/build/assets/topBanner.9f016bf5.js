d77ab33f6f2a29ea51c2946a8ea6e9333b55e488
MwauraKimani <kimmwaus@gmail.com>
2023-04-13 01:07:09 +0300

    home page update

---
+++ public/build/assets/topBanner.9f016bf5.js
@@ -0,0 +1,1 @@
+import{_ as a}from"./_plugin-vue_export-helper.cdc0426e.js";import{o as t,e as n,a as o,t as s}from"./app.8d59652e.js";const _={name:"topBanner",props:["pageName"]},r={class:"page-banner"};function p(c,d,e,i,l,m){return t(),n("div",r,[o("h1",null,s(e.pageName),1)])}const B=a(_,[["render",p],["__scopeId","data-v-04d89f8a"]]);export{B as t};
